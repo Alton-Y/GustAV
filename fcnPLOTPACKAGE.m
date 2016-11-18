168113c69160031ae84c3e177a2ee569e9795b6b
Bill-B <toronto_4@hotmail.com>
2016-11-17 23:06:19 -0500

    added plots to plot package

--- fcnPLOTPACKAGE.m
+++ fcnPLOTPACKAGE.m
@@ -1,14 +1,20 @@
 function [] = fcnPLOTPACKAGE(INFO,FMT,GNDSTN)
 addpath('./PlotPackage')
 
-genfig=figure(1);
-generalinfo(INFO,FMT,GNDSTN,genfig);
+% genfig=figure(1);
+% generalinfo(INFO,FMT,GNDSTN,genfig);
 
-windfig = figure(2);
-winddata(INFO,FMT,GNDSTN,windfig);
+% windfig = figure(2);
+% winddata(INFO,FMT,GNDSTN,windfig);
 
-tunefig = figure(3);
-tuningdata(INFO,FMT,GNDSTN,tunefig);
+% tunefig = figure(3);
+% tuningdata(INFO,FMT,GNDSTN,tunefig);
 
-ekffig = figure(4);
-ekfexplore(INFO,FMT,GNDSTN,ekffig);
+% ekffig = figure(4);
+% ekfexplore(INFO,FMT,GNDSTN,ekffig);
+
+% tecsfig = figure(5);
+% tecsexplore(INFO,FMT,GNDSTN,tecsfig);
+
+% battfig = figure(6);
+% batterydata(INFO,FMT,GNDSTN,battfig);
