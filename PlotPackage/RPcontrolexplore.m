10fb3645118acb0df7e60b3412464288e670452f
Bill-B <billbissonn@gmail.com>
2018-01-27 23:47:53 -0500

    changing auto tune figure to include more roll and pitch controller data

--- PlotPackage/RPcontrolexplore.m
+++ PlotPackage/RPcontrolexplore.m
@@ -1,7 +1,7 @@
-function [] = tuningdata(INFO,FMT,GND,fig)
-%plots autotune info
+function [] = RPcontrolexplore(INFO,FMT,GND,fig)
+%plots info related to Roll/Pitch controllers
 
-fig.Name = 'AutoTune Info';
+fig.Name = 'Roll/Pitch Controller Info';
 clf(fig);
 
 s1=subplot(4,1,1);
@@ -16,8 +16,10 @@ ylabel('Rate (deg/s)')
 ax = gca;
 ax.YColor = 'k';
 yyaxis right
+hold on
 try
     p=plot(FMT.ATRP.TimeS(FMT.ATRP.Type == 0),FMT.ATRP.P(FMT.ATRP.Type == 0),'-k');
+%     i=plot(FMT.PIDR.TimeS,FMT.PIDR.P,'--r');
     legend([dem,ach,p],{'Demanded Roll Rate','Achieved Roll Rate','P Gain'},'location','northwest')
 catch
 end
@@ -34,6 +36,7 @@ grid on
 box on
 
 s2=subplot(4,1,2);
+yyaxis left
 hold on
 mode = fcnGETMODE(INFO,FMT.CTUN.TimeS);
 temp = FMT.CTUN.NavRoll;
@@ -44,14 +47,26 @@ temp = FMT.CTUN.Roll;
 temp(mode == 0) = nan;
 ach=plot(FMT.CTUN.TimeS,temp,'-b');
 clear temp
-legend([dem,ach],{'Demanded Roll','Achieved Roll'},'location','northwest')
+ax = gca;
+ax.YColor = 'k';
 ylabel('Angle (deg)')
-% set(gca,'XTickMode','manual');
-% datetick('x','HH:MM:SS')
 axis tight
-% set(gca,'XTickMode','manual');
 grid on
 box on
+
+yyaxis right
+hold on
+
+Ip=plot(FMT.PIDR.TimeS,FMT.PIDR.I,'--k');
+ylabel('Integrator');
+legend([dem,ach,Ip],{'Demanded Roll','Achieved Roll','Roll Integrator'},'location','northwest')
+ax = gca;
+ax.YColor = 'k';
+% set(gca,'XTickMode','manual');
+% datetick('x','HH:MM:SS')
+
+% set(gca,'XTickMode','manual');
+
 
 s3=subplot(4,1,3);
 hold on
@@ -82,6 +97,7 @@ grid on
 box on
 
 s4= subplot(4,1,4);
+yyaxis left
 hold on
 temp = FMT.CTUN.NavPitch;
 temp(mode == 0) = nan;
@@ -90,14 +106,24 @@ clear temp
 temp = FMT.CTUN.Pitch;
 temp(mode == 0) = nan;
 ach=plot(FMT.CTUN.TimeS,temp,'-b');
-legend([dem,ach],{'Demanded Pitch','Achieved Pitch'},'location','northwest')
 ylabel('Angle (deg)')
 % set(gca,'XTickMode','manual');
 % datetick('x','HH:MM:SS')
 axis tight
 grid on
 box on
+ax = gca;
+ax.YColor = 'k';
 
+yyaxis right
+hold on
+Ip=plot(FMT.PIDP.TimeS,FMT.PIDP.I,'--k');
+axis tight
+ax = gca;
+ax.YColor = 'k';
+ylabel('Integrator');
+
+legend([dem,ach,Ip],{'Demanded Pitch','Achieved Pitch','Pitch Integrator'},'location','northwest')
 linkaxes([s1,s2,s3,s4],'x');
 try
 xlim([min(INFO.flight.startTimeS),max(INFO.flight.endTimeS)]);
--- fcnPLOTPACKAGE.m
+++ fcnPLOTPACKAGE.m
@@ -3,27 +3,28 @@ addpath('./PlotPackage')
 
 genfig=figure(1);
 generalinfo(INFO,FMT,GND,AVT,genfig);
-% % 
-% windfig = figure(2);
-% winddata(INFO,FMT,GND,AVT,windfig);
-% % 
-% tunefig = figure(3);
-% tuningdata(INFO,FMT,GND,tunefig);
-% % % 
+
+windfig = figure(2);
+winddata(INFO,FMT,GND,AVT,windfig);
+
+rollpitchcontrolfig = figure(3);
+RPcontrolexplore(INFO,FMT,GND,rollpitchcontrolfig);
+
 channelfig = figure(4);
 channeldata(INFO,FMT,GND,channelfig);
-% 
-% ekffig = figure(5);
-% ekfexplore(INFO,FMT,GND,ekffig);
-% 
-% tecsfig = figure(6);
-% tecsexplore(INFO,FMT,GND,tecsfig);
-% 
-% battfig = figure(7);
-% batterydata(INFO,FMT,GND,battfig);
-% 
-% posfig = figure(8);
-% position(INFO,FMT,GND,AVT,posfig);
+
+ekffig = figure(5);
+ekfexplore(INFO,FMT,GND,ekffig);
+
+tecsfig = figure(6);
+tecsexplore(INFO,FMT,GND,tecsfig);
+
+battfig = figure(7);
+batterydata(INFO,FMT,GND,battfig);
+
+posfig = figure(8);
+position(INFO,FMT,GND,AVT,posfig);
 
 motorfig = figure(9);
 motordata(INFO,FMT,motorfig);
+
