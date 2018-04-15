757ca3de31e1c6f6652175433e574db03d1d8c08
Bill-B <billbissonn@gmail.com>
2018-04-15 18:05:04 -0400

    battarydata: adding resting voltage approximation when avail.

--- PlotPackage/batterydata.m
+++ PlotPackage/batterydata.m
@@ -6,21 +6,23 @@ clf(fig);
 s1=subplot(3,1,1);
 hold on
 try
-v=plot(FMT.BAT.TimeS,FMT.BAT.Volt,'-k');
+    v=plot(FMT.BAT.TimeS,FMT.BAT.Volt,'-k');
+    vr = plot(FMT.BAT.TimeS,FMT.BAT.VoltR,'.k','MarkerSize',2);    
 catch
-v=plot(FMT.CURR.TimeS,FMT.CURR.Volt,'-k');
+    v=plot(FMT.CURR.TimeS,FMT.CURR.Volt,'-k');
 end
 t1a = s1.YLim(1):s1.YLim(2); %create left axis y-tick before axis tight
 
 if isfield(FMT,'CUR2')==1 || isfield(FMT,'BAT2')==1
     yyaxis right
     try
-    v2=plot(FMT.BAT2.TimeS,FMT.BAT2.Volt,'-b');    
+        v2=plot(FMT.BAT2.TimeS,FMT.BAT2.Volt,'-b');
+        vr2 = plot(FMT.BAT2.TimeS,FMT.BAT2.VoltR,'--b');
     catch
-    v2=plot(FMT.CUR2.TimeS,FMT.CUR2.Volt,'-b');
+        v2=plot(FMT.CUR2.TimeS,FMT.CUR2.Volt,'-b');
     end
     t1b = s1.YLim(1):0.2:s1.YLim(2); %create right axis y-tick before axis tight
-axis tight
+    axis tight
 
 yyaxis left
 s1.YLim(1) = s1.YLim(1)-diff(s1.YLim)/1.5;
@@ -42,9 +44,18 @@ grid minor
 box on
 
 if isfield(FMT,'CUR2')==1 || isfield(FMT,'BAT2')==1
-legend([v,v2],{'Voltage (Main Battery)','Voltage (Avionics Battery)'},'location','northwest');
+    try
+        legend([v,vr,v2,vr2],{'Main Volt','Main Rest. Volt','Second Volt','Second Rest. Volt'},'location','northwest');
+    catch
+        legend([v,v2],{'Main Volt','Second Volt'},'location','northwest');
+    end
 else
-   legend([v],{'Voltage (Main Battery)'},'location','northwest'); 
+    try
+        legend([v,vr],{'Main Volt','Main Rest. Volt,'},'location','northwest');
+    catch
+        
+        legend([v],{'Main Volt'},'location','northwest');
+    end
 end
 
 s2=subplot(3,1,2);
@@ -86,9 +97,9 @@ grid on
 grid minor
 box on
 if isfield(FMT,'CUR2')==1 || isfield(FMT,'BAT2')==1
-legend([a,a2],{'Amps (Main Battery)','Amps (Avionics Battery)'},'location','northwest');
+legend([a,a2],{'Main Curr','Second CUrr'},'location','northwest');
 else
-    legend([a],{'Amps (Main Battery)'},'location','northwest');
+    legend([a],{'Main Curr'},'location','northwest');
 end
 
 
@@ -110,9 +121,9 @@ try
 catch
 a2=plot(FMT.CUR2.TimeS,FMT.CUR2.CurrTot,'-b');
 end
-legend([a,a2],{'Total Draw (Main Battery)','Total Draw (Avionics Battery)'},'location','northwest');
+legend([a,a2],{'Main Total Draw','Second Total Draw'},'location','northwest');
 else
-   legend([a],{'Total Draw (Main Battery)'},'location','northwest'); 
+   legend([a],{'Main Total Draw'},'location','northwest'); 
 end
 ylabel('mAh');
 axis tight
