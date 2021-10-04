7dad0383f8917d26fb3925c3cb7c6b130bd9fb7d
Bill-B <billbissonn@gmail.com>
2021-10-04 15:17:04 -0400

    adding nav explore plots

---
+++ PlotPackage/navexplore.m
@@ -0,0 +1,87 @@
+function navexplore(INFO,FMT,fig)
+fig.Name = 'NAV Explore';
+clf(fig);
+try
+    s1=subplot(5,1,1);
+    hold on
+%     hdem=plot(FMT.NTUN.TimeS,wrapTo360(FMT.NTUN.TBrg),'-k');
+    h=plot(FMT.NTUN.TimeS,wrapTo360(FMT.NTUN.NavBrg),'.');
+%     x = plot(FMT.NTUN.TimeS,FMT.NTUN.XT,'--r');
+    x = plot(FMT.GPS.TimeS,FMT.GPS.GCrs,'.');
+    heading = plot(FMT.ATT.TimeS,FMT.ATT.Yaw,'.');
+    
+    legend([h,x,heading],{'Nav Brg','GPS Course','Heading'},'location','northwest');
+    ylabel('deg');
+    axis tight
+    grid on
+    box on
+catch
+end
+s2=subplot(5,1,2);
+
+hold on
+nroll = plot(FMT.CTUN.TimeS,FMT.CTUN.NavRoll,'.');
+roll = plot(FMT.CTUN.TimeS,FMT.CTUN.Roll,'.');
+    grid on
+    box on
+
+legend('Nav Roll','Roll')
+s3=subplot(5,1,4);
+%airspeed
+% yyaxis left
+hold on
+arsp = plot(FMT.CTUN.TimeS,FMT.CTUN.Aspd,'.');
+arsp_g = plot(FMT.GPS.TimeS,FMT.GPS.Spd,'.');
+% arsp_avt = plot(AVT.OUT.TimeS, AVT.OUT.ARSP, '-r');
+
+
+axis tight
+ylabel('m/s');
+% ax = gca;
+% ax.YColor = 'k';
+legend([arsp,arsp_g],{'Airspeed','GND Speed'});
+grid on 
+box on
+
+s4=subplot(5,1,3);
+
+
+hold on
+dem=plot(FMT.RCOU.TimeS,FMT.RCOU.C1,'.');
+
+legend([dem],{'L AIL (+TE UP)'});
+
+axis tight
+grid on
+box on
+
+s5=subplot(5,1,5);
+
+
+hold on
+dem=plot(FMT.NKF1.TimeS,-FMT.NKF1.VD,'.');
+
+
+accel=smooth(FMT.IMU.AccX,1,'lowess') -...
+    (9.80665.*sind( interp1(FMT.ATT.TimeS,smooth(FMT.ATT.Pitch,1,'lowess'),FMT.IMU.TimeS)));
+
+aplot=plot(FMT.IMU.TimeS,accel,'.');
+
+
+legend([dem,aplot],{'Climb Rate m/s','X accel m/s/s'});
+
+axis tight
+grid on
+box on
+
+
+
+try
+    linkaxes([s1,s2,s3,s4,s5],'x');
+end
+try
+    xlim([min(INFO.flight.startTimeS),max(INFO.flight.endTimeS)]);
+catch
+    axis tight
+end
+clear s1 s2 s3 s4 s5
--- fcnPLOTPACKAGE.m
+++ fcnPLOTPACKAGE.m
@@ -1,56 +1,59 @@
 function [] = fcnPLOTPACKAGE(INFO,FMT,GND,AVT,TLOG,TEMPLOG)
 addpath('./PlotPackage')
+% % 
+genfig=figure(1);
+generalinfo(INFO,FMT,GND,AVT,genfig);
+
+windfig = figure(2);
+winddata(INFO,FMT,GND,AVT,windfig);
 % 
-% genfig=figure(1);
-% generalinfo(INFO,FMT,GND,AVT,genfig);
-% 
-% windfig = figure(2);
-% winddata(INFO,FMT,GND,AVT,windfig);
-% 
-% rollpitchcontrolfig = figure(3);
-% RPcontrolexplore(INFO,FMT,GND,rollpitchcontrolfig);
-% 
-% channelfig = figure(4);
-% channeldata(INFO,FMT,GND,channelfig);
-% 
-% ekffig = figure(5);
-% ekfexplore(INFO,FMT,GND,ekffig);
-% 
-% tecsfig = figure(6);
-% tecsexplore(INFO,FMT,GND,tecsfig);
-% 
+rollpitchcontrolfig = figure(3);
+RPcontrolexplore(INFO,FMT,GND,rollpitchcontrolfig);
+
+channelfig = figure(4);
+channeldata(INFO,FMT,GND,channelfig);
+
+ekffig = figure(5);
+ekfexplore(INFO,FMT,GND,ekffig);
+
+tecsfig = figure(6);
+tecsexplore(INFO,FMT,GND,tecsfig);
+
 battfig = figure(7);
-batterydata(INFO,FMT,GND,battfig);
-% 
-% posfig = figure(8);
-% position(INFO,FMT,GND,AVT,posfig);
-% % 
-% motorfig = figure(9);
-% motordata(INFO,FMT,motorfig);
-% 
-% telemfig = figure(10);
-% rssidata(INFO,FMT,TLOG,telemfig);
-% 
-% hardwarefig = figure(11);
-% hardware(INFO,FMT,TLOG,hardwarefig);
-% 
-% sysidfig = figure(12);
-% sysidexplore(INFO,FMT,sysidfig);
+batterydata(INFO,FMT,GND,TEMPLOG,battfig);
 
-% airspeedfig = figure(13);
-% airspeedexplore(INFO,FMT,airspeedfig);
+posfig = figure(8);
+position(INFO,FMT,GND,AVT,posfig);
+% 
+motorfig = figure(9);
+motordata(INFO,FMT,motorfig);
 
-% altfig = figure(14);
-% altitudeexplore(INFO,FMT,altfig);
+telemfig = figure(10);
+rssidata(INFO,FMT,TLOG,telemfig);
+
+hardwarefig = figure(11);
+hardware(INFO,FMT,TLOG,hardwarefig);
+
+sysidfig = figure(12);
+sysidexplore(INFO,FMT,sysidfig);
+
+airspeedfig = figure(13);
+airspeedexplore(INFO,FMT,airspeedfig);
+
+altfig = figure(14);
+altitudeexplore(INFO,FMT,altfig);
+
+logfig = figure(15);
+logging(INFO,FMT,TLOG,logfig);
+
+fig = figure(16)
+batterydata2(INFO,FMT,GND,fig)
 % 
-% logfig = figure(15);
-% logging(INFO,FMT,TLOG,logfig);
-% 
-% fig = figure(16)
-% batterydata2(INFO,FMT,GND,fig)
-% % 
-% temperaturefig = figure(17)
-% temperaturedata(INFO,FMT,GND,TEMPLOG,temperaturefig);
-% 
-% fig = figure(18)
-% chargingdata(INFO,FMT,GND,TEMPLOG,fig)
+temperaturefig = figure(17)
+temperaturedata(INFO,FMT,GND,TEMPLOG,temperaturefig);
+
+fig = figure(18)
+chargingdata(INFO,FMT,GND,TEMPLOG,fig)
+
+fig = figure(19)
+navexplore(INFO,FMT,fig)
