5320f54d5b3cfc8d5b67fd8a565b46e058fa46b5
CREATeV-GND <raalf@ryerson.ca>
2023-09-19 17:51:59 -0400

    rpcontrolexplore: adding calculation of servo out from controller

--- PlotPackage/RPcontrolexplorePITCH.m
+++ PlotPackage/RPcontrolexplorePITCH.m
@@ -6,54 +6,61 @@ clf(fig);
 
 s1=subplot(4,1,1);
 hold on
-plot(FMT.PIDP.TimeS,FMT.PIDP.Act)
-plot(FMT.PIDP.TimeS,FMT.PIDP.Tar)
+plot(FMT.PIDP.TimeS,FMT.PIDP.Act,'.')
+plot(FMT.PIDP.TimeS,FMT.PIDP.Tar,'.')
 ylabel('Rate')
 legend('Act','Target','Location','best')
 grid on
 
 axis tight
 
-xlim([2936 2947])
+% xlim([2936 2947])
+
+% s2=subplot(4,1,2);
+% hold on
+% plot(FMT.PIDP.TimeS,FMT.PIDP.Err,'.')
+% ylabel('Rate')
+% legend('Err','Location','best')
+% grid on
+% 
+% axis tight
+
+% xlim([2936 2947])
+
 
 s2=subplot(4,1,2);
 hold on
-plot(FMT.PIDP.TimeS,FMT.PIDP.Err)
-ylabel('Rate')
-legend('Err','Location','best')
-grid on
-
-axis tight
-
-xlim([2936 2947])
-
-
-s3=subplot(4,1,3);
-hold on
-plot(FMT.PIDP.TimeS,FMT.PIDP.FF)
-plot(FMT.PIDP.TimeS,FMT.PIDP.P)
-plot(FMT.PIDP.TimeS,FMT.PIDP.D)
-plot(FMT.PIDP.TimeS,FMT.PIDP.I)
+plot(FMT.PIDP.TimeS,FMT.PIDP.FF,'.')
+plot(FMT.PIDP.TimeS,FMT.PIDP.P,'.')
+plot(FMT.PIDP.TimeS,FMT.PIDP.D,'.')
+plot(FMT.PIDP.TimeS,FMT.PIDP.I,'.')
 ylabel('Rate')
 legend('FF','P','D','I','Location','best')
 grid on
 
 axis tight
 
-xlim([2936 2947])
+% xlim([2936 2947])
 
-s4=subplot(4,1,4);
+s3=subplot(4,1,3);
 hold on
-plot(FMT.AETR.TimeS,FMT.AETR.Elev./(4500))
+plot(FMT.AETR.TimeS,FMT.AETR.Elev./(4500),'.')
 ylabel('ELEV')
 yyaxis right
-plot(FMT.RCOU.TimeS,FMT.RCOU.C2)
+plot(FMT.RCOU.TimeS,FMT.RCOU.C2,'.')
 ylabel('PWM')
 % legend('FF','P','D','Location','best')
 grid on
 
 axis tight
 
-xlim([2936 2947])
+% xlim([2936 2947])
+
+s4= subplot(4,1,4)
+hold on
+plot(FMT.PIDP.TimeS,FMT.PIDP.Dmod)
+grid on
+
+axis tight
 
 linkaxes([s1,s2,s3,s4],'x');
--- PlotPackage/RPcontrolexploreROLL.m
+++ PlotPackage/RPcontrolexploreROLL.m
@@ -6,54 +6,80 @@ clf(fig);
 
 s1=subplot(4,1,1);
 hold on
-plot(FMT.PIDR.TimeS,FMT.PIDR.Act)
-plot(FMT.PIDR.TimeS,FMT.PIDR.Tar)
+plot(FMT.PIDR.TimeS,FMT.PIDR.Act,'.')
+plot(FMT.PIDR.TimeS,FMT.PIDR.Tar,'.')
 ylabel('Rate')
 legend('Act','Target','Location','best')
 grid on
 
 axis tight
 
-xlim([2936 2947])
+% xlim([2936 2947])
+
+% s2=subplot(4,1,2);
+% hold on
+% plot(FMT.PIDR.TimeS,FMT.PIDR.Err,'.')
+% ylabel('Rate')
+% legend('Err','Location','best')
+% grid on
+% 
+% axis tight
+
+% xlim([2936 2947])
+
 
 s2=subplot(4,1,2);
 hold on
-plot(FMT.PIDR.TimeS,FMT.PIDR.Err)
-ylabel('Rate')
-legend('Err','Location','best')
-grid on
-
-axis tight
-
-xlim([2936 2947])
-
-
-s3=subplot(4,1,3);
-hold on
-plot(FMT.PIDR.TimeS,FMT.PIDR.FF)
-plot(FMT.PIDR.TimeS,FMT.PIDR.P)
-plot(FMT.PIDR.TimeS,FMT.PIDR.D)
-plot(FMT.PIDR.TimeS,FMT.PIDR.I)
+plot(FMT.PIDR.TimeS,FMT.PIDR.FF,'.')
+plot(FMT.PIDR.TimeS,FMT.PIDR.P,'.')
+plot(FMT.PIDR.TimeS,FMT.PIDR.D,'.')
+plot(FMT.PIDR.TimeS,FMT.PIDR.I,'.')
 ylabel('Rate')
 legend('FF','P','D','I','Location','best')
 grid on
 
 axis tight
 
-xlim([2936 2947])
+% xlim([2936 2947])
 
-s4=subplot(4,1,4);
+
+scaled_value = (FMT.PIDR.FF + FMT.PIDR.P  + FMT.PIDR.I).*100;
+scaled_value(scaled_value>4500)=4500;
+scaled_value(scaled_value<-4500)=-4500;
+
+pwmout = 1521+ ((scaled_value.*(2070-1521))./4500);
+pwmout(scaled_value <0) = 1521-((-scaled_value(scaled_value<0)*(1521-925))./4500);
+%  if (scaled_value > 0) {
+%         return servo_trim + uint16_t( (scaled_value * (float)(servo_max - servo_trim)) / (float)high_out);
+%     } else {
+%         return servo_trim - uint16_t( (-scaled_value * (float)(servo_trim - servo_min)) / (float)high_out);
+%     }
+% xlim([2936 2947])
+
+
+s3=subplot(4,1,3);
 hold on
-plot(FMT.AETR.TimeS,FMT.AETR.Ail./(4500))
-ylabel('AIL')
-yyaxis right
-plot(FMT.RCOU.TimeS,FMT.RCOU.C2)
+
 ylabel('PWM')
+
+plot(FMT.RCOU.TimeS,FMT.RCOU.C1,'.')
+% plot(FMT.PIDR.TimeS,pwmout,'.' );
+
+% yyaxis right
+% ylabel('AIL')
+% plot(FMT.AETR.TimeS,FMT.AETR.Ail./(4500),'.')
+
 % legend('FF','P','D','Location','best')
 grid on
 
 axis tight
 
-xlim([2936 2947])
+s4= subplot(4,1,4)
+hold on
+plot(FMT.PIDR.TimeS,FMT.PIDR.Dmod)
+grid on
+yyaxis right
+plot(FMT.PIDR.TimeS,FMT.PIDR.SRate)
+axis tight
 
 linkaxes([s1,s2,s3,s4],'x');
